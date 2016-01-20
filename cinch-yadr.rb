ab898af46cbb7f2ebb02f8e498018bf3abab0899
Chase Hallsted <challst@L-HOAG734G3Q-M.local>
2016-01-20 10:59:25 -0600

    Formatting change, didnt change any functionality

--- cinch-yadr.rb
+++ cinch-yadr.rb
@@ -8,10 +8,10 @@ class Yadr
 
     def execute(m,format)
         dice   = DiceBag::Roll.new(format)
-        
+
         result = dice.result()
-        tally =result.sections[0].tally()
-        m.reply "result: #{result}",true
-        m.reply "breakout: #{tally}",true
+        tally  = result.sections[0].tally()
+        m.reply "Result  : #{result}",true
+        m.reply "Breakout: #{tally}",true
     end
 end

9d6f25264fcf4818caffac3cb994ce5f820a2af6
Chase Hallsted <chase.hallsted@gmail.com>
2016-01-21 10:17:20 -0600

    Updated Regex
    To allow for non-case-sensitive matching of the "d"
    To allow for the "number" of die to be optional

--- cinch-yadr.rb
+++ cinch-yadr.rb
@@ -4,13 +4,12 @@ require 'dicebag'
 class Yadr
     include Cinch::Plugin
 
-    match /roll (\d{1,2}d\d{1,2})/, method: :roll
-    match /roll (\d{1,2}D\d{1,2})/, method: :roll
+    match /roll (\d?{1,2}[dD]\d{1,2})/, method: :roll
     match /lazy/, method: :lazyroll
     match /help/
 
     def roll(m,format)
-        m.reply 'Rolling #{format}'
+        m.reply "Rolling #{format}"
         dice   = DiceBag::Roll.new(format)
         result = dice.result()
         tally  = result.sections[0].tally()
@@ -19,15 +18,15 @@ class Yadr
     end
 
     def lazyroll(m)
-        m.reply 'Rolling 1D20'
+        m.reply "Rolling 1D20"
         dice   = DiceBag::Roll.new('1d20')
         result = dice.result()
         m.reply "Result  : #{result}",true
     end
 
     def execute(m)
-        m.reply '!roll <rolls>d<sides> - Rolls <sides> die <rolls> times'
-        m.reply '!lazy                 - Rolls one D20'
-        m.reply '!help                 - Shows this Message'
+        m.reply "!roll <rolls>d<sides> - Rolls <sides> die <rolls> times"
+        m.reply "!lazy                 - Rolls 1D20"
+        m.reply "!help                 - Shows this Message"
     end
 end

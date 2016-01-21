40504215e4fe79717934820adee88997aa5d1113
bbourqu <brett@bourqu.in>
2016-01-20 20:47:46 -0600

    Added the lazy roll

--- cinch-yadr.rb
+++ cinch-yadr.rb
@@ -4,14 +4,29 @@ require 'dicebag'
 class Yadr
     include Cinch::Plugin
 
-    match(/roll (\d{1,2}d\d{1,2})/)
-
-    def execute(m,format)
+    match /roll (\d{1,2}d\d{1,2})/, method: :roll
+    match /lazy/, method: :lazyroll
+    match /help/ 
+    def roll(m,format)
         dice   = DiceBag::Roll.new(format)
 
         result = dice.result()
         tally  = result.sections[0].tally()
         m.reply "Result  : #{result}",true
         m.reply "Breakout: #{tally}",true
+    end
+   
+    def lazyroll(m)
+        m.reply 'Rolling 1d20'
+        dice   = DiceBag::Roll.new('1d20')
+        result = dice.result()
+        tally  = result.sections[0].tally()
+        m.reply "Result  : #{result}",true
+        m.reply "Breakout: #{tally}",true
+    end
+
+    def execute(m)
+        m.reply '!roll <rolls>d<sides> - Rolls <sides> die <rolls> times',true
+        m.reply '!lazy - Rolls one D20',true
     end
 end

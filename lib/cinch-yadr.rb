5e3e780ef797cd8cc04c337c6e1dc1c8cc0e918f
Chase Hallsted <chase.hallsted@gmail.com>
2016-02-19 23:02:09 -0600

    Trying to make this pass a few basic RSpec Tests, this is currently failing

--- lib/cinch-yadr.rb
+++ lib/cinch-yadr.rb
@@ -9,35 +9,31 @@ require 'dicebag'
 class Yadr
   include Cinch::Plugin
 
-  on :message, /roll (\d?{1,2}[dD]\d{1,2})/ do
-    :roll
-  end
-  on :message, /lazy/ do
-    :lazyroll
-  end
-  on :message, /help/ do
-    :execute
-  end
-
   def roll(m, format)
-    m.reply "Rolling #{format} for", true
-    dice    = DiceBag::Roll.new(format)
-    result  = dice.result()
-    tally   = result.sections[0].tally()
-    m.reply "Total   : #{result}", true
-    m.reply "Breakout: #{tally}", true if tally.length > 1
+    on :message, /roll (\d?{1,2}[dD]\d{1,2})/ do
+      m.reply "Rolling #{format} for", true
+      dice    = DiceBag::Roll.new(format)
+      result  = dice.result()
+      tally   = result.sections[0].tally()
+      m.reply "Total   : #{result}", true
+      m.reply "Breakout: #{tally}", true if tally.length > 1
+    end
   end
 
   def lazyroll(m)
-    m.reply 'Rolling 1D20'
-    dice    = DiceBag::Roll.new('1d20')
-    result  = dice.result()
-    m.reply "Result  : #{result}", true
+    on :message, /lazy/ do
+      m.reply 'Rolling 1D20'
+      dice    = DiceBag::Roll.new('1d20')
+      result  = dice.result()
+      m.reply "Result  : #{result}", true
+    end
   end
 
   def execute(m)
-    m.reply '!roll <rolls>d<sides> - Rolls <sides> die <rolls> times'
-    m.reply '!help                 - Shows this Message'
-    m.reply '!lazy                 - Rolls 1D20'
+    on :message, /help/ do
+      m.reply '!roll <rolls>d<sides> - Rolls <sides> die <rolls> times'
+      m.reply '!help                 - Shows this Message'
+      m.reply '!lazy                 - Rolls 1D20'
+    end
   end
 end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,6 @@
+gem 'rspec', '~> 3.4'
+require 'rspec'
+require 'cinch-yadr'
+require 'cinch/test'
+
+include Cinch::Test
